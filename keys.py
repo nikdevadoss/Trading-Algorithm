4a413da77b1855d8b323bdfd9c44ab76c65e8f97
Nikhil Devadoss <44107389+nikdevadoss@users.noreply.github.com>
2020-06-12 15:12:36 -0700

    Delete keys.py

--- keys.py
+++
@@ -1,9 +0,0 @@
-alpacaPaperKey = {'api_key': 'PKT4CE3911A2ZOG82SAL', 'secret_key': 'Q44Rc/9uNL6Z/tGrCb3QuydZ3znbydkzzE2nHv4N'}
-
-twilioKey = {'sid': 'AC85bd85daff99c8526c6ef33c8b0293a7', 'auth_token': '6c2991ee43c5f7e30b4958005e65f256'}
-
-robinhoodKey = {'username': 'nikdevadoss@gmail.com', 'password': 'Mariamymom94' , 'qr_code': 'DJ224CLXBCUS4L76'}
-
-coinbaseKey = {'public_key': 'SXLRXCSbNDwNWErR', 'private_key' : 'IIR9LhFR8QFe0qLYQTMOCL5u6RB57kmD', 'password': 'Mariamymom94'}
-
-personalInfo = {'senderEmail': "nikdevtrading@gmail.com", 'senderPass': "nikdev123", "recieverEmail": "nikdevtrading@gmail.com", "phoneNumber": "+16505563279", "twilioNumber": "+17722720874"}
